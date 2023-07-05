262e7978cb43b373acdf749d1f89ad57c82a88be
AmzylMohcine <amzylmohcine@gmail.com>
2023-07-05 15:06:48 +0200

    NodeJs - Mongo

---
+++ medium-f2i-main/controllers/articleController.js
@@ -0,0 +1,70 @@
+const Article = require("../models/articleModel")
+
+const create = async (req, res) => {
+  const { title, content } = req.body
+  try {
+    const article = await Article({ title, content })
+    article.save().then(saveArticle => {
+      res.status(201).json(saveArticle)
+    })
+  } catch (error) {
+    res.status(500).json({ error: `Erreur lors de la sauvegarde de l'article` })
+  }
+}
+const showArticle = async (req, res) => {
+  const id = req.params.id
+  try {
+    const article = await Article.findOne({ _id: id })
+    res.status(200).render("show", { article: article })
+  } catch (error) {
+    res.json({ message: "Article non trouvé" })
+  }
+}
+
+const showAll = async (req, res) => {
+  try {
+    const article = await Article.find()
+    res.status(200).render("list", { article: article })
+  } catch (error) {
+    res.json({ message: "Article non trouvé" })
+  }
+}
+
+const comments = async (req, res) => {
+  try {
+    const { id } = req.params
+    const { author, content } = req.body
+    await Article.findById(id)
+      .then(article => {
+        if (!article) {
+          return res.status(404).json({ error: "Article introuvable" })
+        }
+        const comment = { author, content }
+        article.comments.push(comment)
+        return article.save()
+      })
+      .then(updateArticle => {
+        res.json(updateArticle)
+      })
+  } catch (error) {
+    res.status(500).json({ error: `Erreur lors de l'ajout du commentaire` })
+  }
+}
+
+const applaud = async (req, res) => {
+  try {
+    const { id } = req.params
+    await Article.findById(id)
+      .then(article => {
+        if (!article) {
+          return res.status(404).json({ error: "Article introuvable" })
+        }
+        article.applauseCount++
+        return article.save()
+      })
+      .then(updateArticle => res.json(updateArticle))
+  } catch (error) {
+    res.status(500).json({ error: `Erreur lors de l'ajoout d'applaudissements` })
+  }
+}
+module.exports = { create, comments, applaud, showArticle, showAll }
---
+++ medium-f2i-main/routes/articleRoutes.js
@@ -0,0 +1,24 @@
+const { create, comments, applaud, showArticle, showAll } = require("../controllers/articleController")
+const Article = require("../models/articleModel")
+
+const router = require("express").Router()
+
+router.get("/create", (req, res) => {
+  res.render("create")
+})
+router.get("/:id/show", showArticle)
+router.get("/list", showAll)
+router.post("/create", create)
+router.get("/:id/comments", async (req, res) => {
+  try {
+    const id = req.params.id
+    const article = await Article.findById({ _id: id })
+    res.render("comments", { article })
+  } catch (error) {
+    res.status(404).json({ error: error.message })
+  }
+})
+router.post("/:id/comments", comments)
+router.post("/:id/applaud", applaud)
+
+module.exports = router
